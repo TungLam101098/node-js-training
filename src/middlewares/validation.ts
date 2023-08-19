80cbac430ed5c96719b36bd2a1dfe05e04c0e8f3
TungLam101098 <n.t.lam10101998@gmail.com>
2023-08-19 23:33:15 +0700

    refactor(api): update dto

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,5 +1,3 @@
-import { plainToClass } from 'class-transformer';
-// import { validate, ValidationError } from 'class-validator';
 import * as express from 'express';
 import { BaseSchema, ValiError, parse, flatten } from 'valibot';
 
